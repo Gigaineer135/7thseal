5a8cf8af81d6ecfef9af1bd2cd7ace9ebebf9fa1
Gigaineer135 <80706233+Gigaineer135@users.noreply.github.com>
2021-04-30 08:28:39 -0400

    Add files via upload

--- SetUp.sh
+++ SetUp.sh
@@ -1,7 +1,43 @@
 #!/usr/bin/bash
+'''
+Dev: Surgat
+
+                                                     @#@@@@@@@#@
+                                                &@@,@           @,@@&
+                                             @@.                     ,@@
+                                           @&                           @@
+                                         %@
+                                        &@
+                                        @
+                                       &@
+                @@@@@@                 &@
+            .@@*      @@&              &@
+          *@             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+         /@                            &@
+         &@                            &@
+         .@                            &@
+           @&           .@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+             #@@%,   @@@               &@
+                  @@@@                 &@
+                                       &@
+                                       &@                                     %@@
+                                       &@                  @@                    @*
+                                       &@                 @. @*                  #@
+                                       &@               ,@.   @#                .@,
+                                       &@@@@@@@@@@@@@@@/@@@@@@@@&@@@@@@@@@@@@@@@@#
+                                                      #@        @@
+                                                     @@          &@
+                                                    @@            (@
+                                                    @%@@@@@@@@@@@@@@@
+'''
 apt install gpsd
 apt install gpsd-clients
 apt install python3-pip
 pip3 install tk
 rm -rf /etc/7thseal
 gpsd -D 5 -N -n /dev/ttyUSB0
+mv 7thseal.py /sbin/7thsealconf.py
+echo '#!/usr/bin/bash' > /sbin/7thseal
+echo "python3  /sbin/7thsealconf.py" >> /sbin/7thseal
+chmod 777 /sbin/7thsealconf.py /sbin/7thseal
+chattr +i  /sbin/7thsealconf.py
